7e3ede8ea0551ac5864978d6a94455a64f7a4d7e
Varnitha12345 <129487031+Varnitha12345@users.noreply.github.com>
2024-08-02 11:21:43 +0530

    Project Updated 6

--- src/app/config/mysql.js
+++ src/app/config/mysql.js
@@ -3,7 +3,8 @@ import mysql from 'mysql2/promise';
 const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
-    password: process.env.NODE_ENV === 'development' ? process.env.MYSQL_PASSWORD: process.env.MYSQL_PRO_PASSWORD,
+    // password: process.env.NODE_ENV === 'development' ? process.env.MYSQL_PASSWORD: process.env.MYSQL_PRO_PASSWORD,
+    password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
     waitForConnections: true,
     
